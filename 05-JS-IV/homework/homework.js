7f6a870a15122f96defbef6d1cc9620d9895496c
Andrea Victoria López Palomeque <andreavictorialopezpalomeque@Andreas-MacBook-Air.local>
2022-03-28 01:03:52 -0300

    Homework 4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -166,7 +166,7 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
   producto.calcularPrecioDescuento = function (){
-    return this.precio - (this.precio * this.porcentajeDeDescuento);
+    return this.precio - (this.precio * this.porcentajeDeDescuento); //tambien funciona con producto.precio - (producto.precio * ...)
   };
   return producto;
 }
